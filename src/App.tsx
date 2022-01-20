7c410c6730eed388a3b7b4d902242af4ef0b3af1
jiro4989 <jiroron666@gmail.com>
2022-01-20 22:17:05 +0900

    feat: autoFocusを追加

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ function App() {
         <h2>INPUT</h2>
         <div>
           <p>Set parameters from JIRA URL</p>
-          <input type="text" onChange={(e) => setJIRAURL(e.target.value)} />
+          <input type="text" onChange={(e) => setJIRAURL(e.target.value)} autoFocus />
           <button className="ApplyButton" onClick={(e) => applyParams()}>
             Apply
           </button>
