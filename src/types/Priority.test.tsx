0f70954cf385e01bde304b8b5f99ed5594bfed1e
jiro4989 <jiroron666@gmail.com>
2022-01-22 12:05:26 +0900

    test code

---
+++ src/types/Priority.test.tsx
@@ -0,0 +1,40 @@
+import { priority2Name, priorityHigh, priorityHighest, priorityLow, priorityLowest, priorityMedium } from './Priority'
+
+describe('priority2Name', () => {
+  const testCases = [
+    {
+      description: '正常系: Highest',
+      inPriority: priorityHighest,
+      want: 'Highest',
+    },
+    {
+      description: '正常系: High',
+      inPriority: priorityHigh,
+      want: 'High',
+    },
+    {
+      description: '正常系: Medium',
+      inPriority: priorityMedium,
+      want: 'Medium',
+    },
+    {
+      description: '正常系: Low',
+      inPriority: priorityLow,
+      want: 'Low',
+    },
+    {
+      description: '正常系: Lowest',
+      inPriority: priorityLowest,
+      want: 'Lowest',
+    },
+  ]
+
+  for (const testCase of testCases) {
+    const { description, inPriority, want } = testCase
+
+    test(description, () => {
+      const got = priority2Name(inPriority)
+      expect(got).toEqual(want)
+    })
+  }
+})
