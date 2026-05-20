5c0ebd7a0974fc4ae1bc46c33d8f5d70fde6d26e
jiro4989 <jiroron666@gmail.com>
2026-05-20 20:40:13 +0900

    wip: generateURL を実装

--- public/utils.js
+++ public/utils.js
@@ -1,3 +1,18 @@
-export function sushi() {
-  return "sushi";
+export function generateURL(jira) {
+  const qp = [
+    `pid=${encodeURIComponent(jira.projectID)}`,
+    `issuetype=${encodeURIComponent(jira.issueType)}`,
+    `priority=${encodeURIComponent(jira.priority)}`,
+    `summary=${encodeURIComponent(jira.summary)}`,
+  ]
+  if (jira.description !== '') {
+    qp.push(`description=${encodeURIComponent(jira.description)}`)
+  }
+  jira.labels
+    .map((e) => `labels=${encodeURIComponent(e)}`)
+    .forEach((e) => qp.push(e))
+
+  const queryParams = qp.join('&')
+  const url = `${jira.baseURL}/secure/CreateIssueDetails!init.jspa?${queryParams}`
+  return url
 }
